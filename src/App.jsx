ef913c5f19b7dc6f38e657148e70c245648192b7
prakash18399 <prakashchoudhary1831999@gmail.com>
2024-09-03 11:17:39 +0530

    imgupdated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-          
+          <h1>hello world</h1>
       </div>
         
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,55 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import Card from './Card.jsx'
 import Hero from './Hero.jsx'
 import './index.css'
+import leonbet from './assets/Leonbet.jpg'
+import megapari from './assets/Megapari.jpg'
+import parimatch from './assets/Parimatch.jpg'
+import win1 from './assets/1win.jpg'
+import dafabet from './assets/Dafabet.jpg'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hero />
     <div className="card-container">
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={leonbet}
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={megapari}
           bonus="100% upto Rs. 200,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={parimatch} 
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={win1}
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={dafabet}
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={win1}
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={win1}
           title="Preyash" 
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
           url="https://google.com"/>
 
-    <Card image="https://picsum.photos/200/100" 
+    <Card image={win1}
           title="Preyash" 
           bonus="100% upto Rs. 100,000" 
           li1="Good casino" li2="wow casino" li3="Good casino app" 
